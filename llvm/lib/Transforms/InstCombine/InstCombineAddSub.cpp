20a2cf6883ba62556dab5e4dd37c02c5f2de46d7
Ethan Luis McDonough <emcdonou@qti.qualcomm.com>
2026-09-10 09:55:43 -0700

    p

--- llvm/lib/Transforms/InstCombine/InstCombineAddSub.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineAddSub.cpp
@@ -1967,17 +1967,6 @@ Instruction *InstCombinerImpl::visitAdd(BinaryOperator &I) {
     }
   }
 
-  // X + umax(X, 1) --> umax(X << 1, 1)
-  if (match(&I, m_c_Add(m_Value(A), m_OneUse(m_c_UMax(m_Value(B), m_One())))) &&
-      A == B && (I.hasNoUnsignedWrap() || I.hasNoSignedWrap())) {
-    auto One = ConstantInt::get(Ty, 1);
-    return replaceInstUsesWith(
-        I, Builder.CreateIntrinsic(
-               Intrinsic::umax, {Ty},
-               {One, Builder.CreateShl(A, One, "mul2", I.hasNoUnsignedWrap(),
-                                       I.hasNoSignedWrap())}));
-  }
-
   if (Instruction *R = tryFoldInstWithCtpopWithNot(&I))
     return R;
 
@@ -1993,6 +1982,22 @@ Instruction *InstCombinerImpl::visitAdd(BinaryOperator &I) {
       willNotOverflowUnsignedAdd(LHSCache, RHSCache, I)) {
     Changed = true;
     I.setHasNoUnsignedWrap(true);
+  }
+
+  // X + umax(X, 1) --> umax(X << 1, 1) if X + X does not overflow.
+  if (match(&I, m_c_Add(m_Value(A), m_OneUse(m_c_UMax(m_Value(B), m_One())))) &&
+      A == B) {
+    auto Cache = A == LHSCache ? LHSCache : RHSCache;
+    auto One = ConstantInt::get(Ty, 1);
+    bool NoUW =
+        I.hasNoUnsignedWrap() || willNotOverflowUnsignedAdd(Cache, Cache, I);
+    // Do not apply this transformation if the original add might overflow.
+    if (NoUW || I.hasNoSignedWrap())
+      return replaceInstUsesWith(
+          I, Builder.CreateIntrinsic(
+                 Intrinsic::umax, {Ty},
+                 {One, Builder.CreateShl(A, One, "mul2", NoUW,
+                                         I.hasNoSignedWrap())}));
   }
 
   if (Instruction *V = canonicalizeLowbitMask(I, Builder))
